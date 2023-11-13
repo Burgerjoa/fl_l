38c7f8147b8eec9283c6c355f9310981128a152b
Burgerjoa <aikyan@kakao.com>
2023-11-14 00:10:10 +0900

    base settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,35 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "first App",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('first'),
+      ),
+      body: Center(
+          child: Column(
+        children: <Widget>[Text('data'), Text('data'), Text('data')],
+      )),
+    );
+  }
+}
