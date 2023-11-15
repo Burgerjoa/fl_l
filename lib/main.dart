63e9b1acc1cd34b63e3fa576972de3e3143b16be
Burgerjoa <aikyan@kakao.com>
2023-11-15 23:49:48 +0900

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,33 +11,6 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "Characater Card",
-      home: MyCard(),
-    );
-  }
-}
-
-class MyCard extends StatelessWidget {
-  @override
-  const MyCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('first'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-        elevation: 0.0,
-      ),
-      body: const Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Asda"),
-              Text('data'),
-              Text("qsad"),
-            ]),
-      ),
     );
   }
 }
