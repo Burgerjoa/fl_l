9057aa83a20827f76e455256809a736592df9c5a
Burgerjoa <aikyan@kakao.com>
2023-11-16 23:37:06 +0900

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
+String asd = 'asd';
+
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
+  String asd = 'asd';
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "Characater Card",
+      title: asd,
     );
   }
 }
